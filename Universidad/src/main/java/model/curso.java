7e0de37afaa2b29d0aaf64861964e714365f20cc
Mariana-gt200 <marianagiraldotabares@gmail.com>
2026-09-17 15:50:29 -0500

    Tarea poo curso

---
+++ Universidad/src/main/java/model/curso.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.poo.model;
+
+/*
+Esta clase representa el curso de una universidad
+@veriosn 1.0
+@author Mariana Giraldo Tabares
+@fecha 17/09/2026
+ */
+public class curso {
+
+    //Declaracion de atributos
+    //modificador de acceso -> tipo de dato -> nombre de la variable
+    private String nombre;
+    private String codigo;
+
+    //declarar las relaciones
+    private ArrayList<Estudiante>listaEstudiantes;
+}
